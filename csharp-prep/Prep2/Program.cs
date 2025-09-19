0a17ca55f42a7125f116592b3c28f9440cfe705c
corbinwamz <corbinwa@byui.edu>
2025-09-18 21:47:11 -0600

    finished program 9/18/2025

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,60 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string letter = "";
+        Console.Write("What is your grade? ");
+        string input = Console.ReadLine();
+        float grade = int.Parse(input);
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else if (grade < 60)
+        {
+            letter = "F";
+        }
+
+        float lastDigit = grade%10;
+        string symbol = "";
+        if (lastDigit >= 7 && grade >= 97)
+        {
+            symbol = "";
+        }
+        else if (grade < 60)
+        {
+            symbol = "";
+        }
+        else if (lastDigit < 3)
+        {
+            symbol = "-";
+        }
+        else if (lastDigit >= 7)
+        {
+            symbol = "+";
+        }
+
+        Console.WriteLine($"Your letter grade is {letter}{symbol}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Hurray you passed!! Congrats!");
+        }
+        else if (grade < 70)
+        {
+            Console.WriteLine("I'm sorry you failed. Study a little more next time",
+            "you got this!");
+        }
     }
 }
