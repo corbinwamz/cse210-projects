d244a6154d7fea042b9c3d04e68162246b3bd01b
corbinwamz <corbinwa@byui.edu>
2025-09-23 16:47:26 -0600

    changes 9/23/2025

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string play = "yes";
+        while (play == "yes")
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 100);
+            Console.Write("What is your guess? ");
+            string input = Console.ReadLine();
+            int guess = int.Parse(input);
+            int guessNumber = 1;
+            while (guess != magicNumber)
+            {
+
+                if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                Console.Write("What is your guess? ");
+                input = Console.ReadLine();
+                guess = int.Parse(input);
+                guessNumber = guessNumber + 1;
+            }
+            Console.WriteLine("You guessed it!");
+            Console.Write($"You took {guessNumber} tries! Would you want to play again? ");
+            play = Console.ReadLine();
+        }
     }
 }
