41a2fe197f67397e598cd88f2d4a5861b91faf3e
corbinwamz <corbinwa@byui.edu>
2025-09-24 09:09:31 -0600

    In class edits

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.ComponentModel;
+using System.Runtime;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int number = 1;
+        List<int> numbers = new List<int>();
+        int sum = 0;
+        int amount = 0;
+        int max = 0;
+        while (number != 0)
+        {
+            Console.Write("Enter in a number to add to the list. Type 0 when finished: ");
+            string input = Console.ReadLine();
+            number = int.Parse(input);
+            numbers.Add(number);
+        }
+        foreach (int num in numbers)
+        {
+            sum = sum + num;
+            amount = amount + 1;
+            if (num > max)
+            {
+                max = num;
+            }
+        }
+        int avg = sum / amount;
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The Average is: {avg}");
+        Console.WriteLine($"The Highest number is: {max}");
     }
 }
