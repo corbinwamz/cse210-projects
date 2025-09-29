9b27a901bb570a56da8066c75b78f949d44fa336
corbinwamz <corbinwa@byui.edu>
2025-09-29 15:27:44 -0600

    edits 9/29/2025

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,26 @@
 using System;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        Job job2 = new Job();
+        Resume name = new Resume();
+        job1._company = "Meta";
+        job1._jobTitle = "CEO";
+        job1._startYear = 2019;
+        job1._endYear = 2025;
+
+        job2._company = "Apple";
+        job2._jobTitle = "Software Developer";
+        job2._startYear = 2004;
+        job2._endYear = 2017;
+
+        name._name = "Corbin Wamsley";
+        name._jobs.Add(job1);
+        name._jobs.Add(job2);
+        name.printResume();
     }
 }
