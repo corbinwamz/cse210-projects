08fc0ae3823f642b1013ef6a1136e9490d59612e
corbinwamz <corbinwa@byui.edu>
2025-10-06 23:18:37 -0600

    edits 10/6/25

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!!!");
+        List<string> linesToWrite = new List<string>();
+        // linesToWrite = ["Header1,Header2,Header3"];
+        string file = "sandbox.csv";
+        // using (StreamWriter outputFile = new StreamWriter(file))
+        // {
+        //     foreach (string i in linesToWrite)
+        //     {
+        //         string line = string.Join(",", i);
+        //         outputFile.WriteLine(line);
+        //     }
+        // }
+        linesToWrite = ["jan", "jay", "jimmer\n"];
+        string line = string.Join(",", linesToWrite);
+        File.AppendAllText(file, line);
+        linesToWrite = ["6", "7", "8"];
+        line = string.Join(",", linesToWrite);
+        File.AppendAllText(file, line);
     }
 }
