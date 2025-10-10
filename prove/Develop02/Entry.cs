45022877341a081299d2d5057df9ec67f9bf44d5
corbinwamz <corbinwa@byui.edu>
2025-10-10 14:48:36 -0600

    final changes 10/10/25/

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -1,7 +1,6 @@
 public class Entry
 {
-    public string _filename;
-    public string _entries;
-    public string _dateTime;
-    public string _prompt;
+    public string _dateTime { get; set; }
+    public string _prompt { get; set; }
+    public string _entries { get; set; }
 }
--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -1,10 +1,14 @@
 using System.Security.Cryptography.X509Certificates;
+using System.IO;
+using CsvHelper;
+using System.Globalization;
+using CsvHelper.Configuration;
 
 public class Journal
 {
     //public List<Entry> writeEntries = new();
 
-    static List<string> Write(List<string> writeEntries)
+    static List<List<string>> Write(List<List<string>> writeEntries)
     {
         List<string> promptBank = [
             "Who was the most interesting person I interacted with today?",
@@ -21,40 +25,59 @@ public class Journal
         ];
 
         Random randomGenerator = new Random();
-        int number = randomGenerator.Next(1, 5);
+        int number = randomGenerator.Next(0, 10);
         string prompt = promptBank[number];
-        Console.Write($"{prompt}: ");
+        Console.Write($"{prompt}:\n> ");
         string response = Console.ReadLine();
         Entry entry = new();
         entry._dateTime = DateTime.Now.ToString("yyyy-MM-dd");
         entry._prompt = prompt;
         entry._entries = response;
-        writeEntries.Add($"{entry._dateTime}|{entry._prompt}|{entry._entries}\n");
+        writeEntries.Add(new List<string> {$"{entry._dateTime}",$"{entry._prompt}",$"{entry._entries}"});
         return writeEntries;
     }
 
     static string Load()
     {
-        Console.Write("What is your file name? ");
+        Console.Write("What is your file name?\n> ");
         string file = Console.ReadLine();
+        while (!File.Exists($"{file}"))
+        {
+            Console.Write("File does not exist...\n");
+        }
         return file;
     }
 
-    static List<string> Save(List<string> writeEntries)
+    static List<List<string>> Save(List<List<string>> writeEntries)
     {
+        List<Entry> records = new();
         if (writeEntries.Count == 0)
         {
-            Console.WriteLine("You need to write something to save first...");
+            Console.WriteLine("You need to write something to save first...\n");
         }
         else
         {
-            Console.Write("What is the file name: ");
+            Console.Write("What is the file name: \n> ");
             string file = Console.ReadLine();
             // Date,Prompt,Entry
-            foreach (string line in writeEntries)
+            if (!File.Exists($"{file}"))
             {
-                File.AppendAllText(file, line);
+                File.AppendAllText(file, "_dateTime,_prompt,_entries\n");
             }
+                var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+                {
+                    HasHeaderRecord = false,
+                };
+                foreach (List<string> line in writeEntries)
+                {
+                records.Add(new Entry { _dateTime = $"{line[0]}", _prompt = $"{line[1]}", _entries = $"{line[2]}" });
+                }
+                using (var stream = File.Open($"{file}", FileMode.Append))
+                using (var writer = new StreamWriter(stream))
+                using (var csv = new CsvWriter(writer, config))
+                {
+                    csv.WriteRecords(records);
+                }
             writeEntries = [];
         }
         return writeEntries;
@@ -62,18 +85,20 @@ public class Journal
 
     public void DisplayMenu()
     {
-        List<string> writeEntries = [];
+        List<List<string>> writeEntries = [];
         string action = "";
         string file = "";
         while (action != "5")
         {
+            Console.WriteLine("-----------------------------------------------");
             Console.WriteLine("Please select one of the following choices:");
             Console.WriteLine("1. Write");
             Console.WriteLine("2. Display");
             Console.WriteLine("3. Load");
             Console.WriteLine("4. Save");
             Console.WriteLine("5. Quit");
-            Console.Write("What would you like to do? ");
+            Console.WriteLine("-----------------------------------------------");
+            Console.Write("What would you like to do?\n> ");
             action = Console.ReadLine();
             if (action == "1")
             {
@@ -91,29 +116,49 @@ public class Journal
             {
                 writeEntries = Save(writeEntries);
             }
+            else if (action == "5" & writeEntries.Count >=1)
+            {
+                Console.Write("Warning: There are unsaved entries. Are you sure you want to quit? y/n\n> ");
+                string answer = Console.ReadLine();
+                if (answer == "y")
+                {
+                    action = "5";
+                }
+                else
+                {
+                    action = "";
+                }
+            }
         }
     }
 
-    static string Display(List<string> writeEntries, string file)
+    static string Display(List<List<string>> writeEntries, string file)
     {
         if (writeEntries.Count > 0)
         {
-            foreach (string line in writeEntries)
+            foreach (List<string> line in writeEntries)
             {
-                string[] entryInfo = line.Split("|");
-                Console.WriteLine($"Date: {entryInfo[0]} - Prompt: {entryInfo[1]}\n{entryInfo[2]}");
+                Console.WriteLine($"Date: {line[0]} - Prompt: {line[1]}\n{line[2]}");
+                Console.WriteLine("-------------------------------------------------------------------\n");
+
             }
         }
         else if (file == "")
         {
-            Console.WriteLine("You need to load a file or write something first...");
+            Console.WriteLine("You need to load a file or write something first...\n");
         }
         else
         {
-            foreach (string line in File.ReadLines(file))
+            using (var reader = new StreamReader($"..\\Develop02\\{file}"))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
             {
-                string[] entryInfo = line.Split("|");
-                Console.WriteLine($"Date: {entryInfo[0]} - Prompt: {entryInfo[1]}\n{entryInfo[2]}");
+                var info = csv.GetRecords<Entry>().ToList();
+
+                foreach (Entry line in info)
+                {
+                    Console.WriteLine($"Date: {line._dateTime} - Prompt: {line._prompt}\n{line._entries}");
+                    Console.WriteLine("-------------------------------------------------------------------\n");
+                }
             }
         }
         file = "";
