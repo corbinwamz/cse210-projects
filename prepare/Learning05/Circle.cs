fc245e66efd499ce576d54ede9fa2445774d01b4
corbinwamz <corbinwa@byui.edu>
2025-11-12 14:52:30 -0700

    prepare 11/12/15

---
+++ prepare/Learning05/Circle.cs
@@ -0,0 +1,15 @@
+class Circle : Shape
+{
+    private double _radius;
+
+    public Circle(double radius, string color) : base(color)
+    {
+        _radius = radius;
+    }
+
+    public override double GetArea()
+    {
+        double area = Math.PI * Math.Pow(_radius, 2);
+        return area;
+    }
+}
--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.Formats.Asn1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = [];
+
+        double side = 4;
+        string color = "blue";
+        Shape shapeSquare = new Square(side, color);
+        shapes.Add(shapeSquare);
+
+        double length = 15;
+        double width = 10;
+        color = "green";
+        Shape shapeRectangle = new Rectangle(length, width, color);
+        shapes.Add(shapeRectangle);
+
+        double radius = 10;
+        color = "yellow";
+        Shape shapeCircle = new Circle(radius, color);
+        shapes.Add(shapeCircle);
+
+        double area = 0;
+
+        foreach (Shape shape in shapes)
+        {
+            color = shape.GetColor();
+            area = shape.GetArea();
+            Console.WriteLine($"{color} {area}");
+        }
     }
 }
